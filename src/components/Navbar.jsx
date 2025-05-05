a139d6c429c404d14a05495fab2f82be21bcb3a7
PrinceEB-0 <princeonaya07@gmail.com>
2025-05-05 12:53:45 +0300

    Refined App.css and Navbar for EramWear's stylish design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,61 +2,73 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-md navbar-light bg-light shadow-sm mt-1">
-   
+    <nav className="navbar navbar-expand-md bg-white shadow-sm py-3 px-4 border-bottom sticky-top">
+      <div className="container-fluid">
+
         {/* Brand Logo */}
-        <Link to="/" className="navbar-brand fw-bold">
+        <Link to="/" className="navbar-brand fs-4 fw-bold text-dark">
           Eram<span className="text-danger">Wear</span>
         </Link>
 
-       
-
-        {/* Mobile Menu Button */}
+        {/* Mobile Toggle */}
         <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarcontents"
+          aria-controls="navbarcontents"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        {/* Navbar Links */}
+        {/* Collapsible Menu */}
         <div className="collapse navbar-collapse" id="navbarcontents">
-          <ul className="navbar-nav me-auto">
+          <ul className="navbar-nav me-auto mb-2 mb-md-0">
             <li className="nav-item">
-              <b><Link to="/" className="nav-link">Casual Wear</Link></b>
+              <Link to="/" className="nav-link fw-semibold text-dark">
+                Casual Wear
+              </Link>
             </li>
             <li className="nav-item">
-              <b><Link to="/formalwear" className="nav-link">Formal Wear</Link></b>
+              <Link to="/formalwear" className="nav-link fw-semibold text-dark">
+                Formal Wear
+              </Link>
             </li>
             <li className="nav-item">
-              <b><Link to="/addproducts" className="nav-link">Accessories</Link></b>
+              <Link to="/addproducts" className="nav-link fw-semibold text-dark">
+                Accessories
+              </Link>
             </li>
             <li className="nav-item">
-              <b><Link to="/addproducts" className="nav-link">New Arrivals</Link></b>
+              <Link to="/addproducts" className="nav-link fw-semibold text-dark">
+                New Arrivals
+              </Link>
             </li>
           </ul>
 
-          {/* Authorization Links (Aligned Right) */}
-          <ul className="navbar-nav ms-auto">
-          <li className="nav-item">
-              <b><Link to="/aboutus" className="nav-link">About Us</Link></b>
+          <ul className="navbar-nav ms-auto align-items-center">
+            <li className="nav-item me-2">
+              <Link to="/aboutus" className="nav-link text-dark fw-semibold">
+                About Us
+              </Link>
+            </li>
+            <li className="nav-item me-2">
+              <Link to="/signin" className="btn btn-outline-dark px-3">
+                Sign In
+              </Link>
             </li>
             <li className="nav-item">
-              <Link to="/signin" className="btn btn-outline-primary me-2">Sign IN</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/signup" className="btn btn-primary">Sign UP</Link>
+              <Link to="/signup" className="btn btn-danger px-3">
+                Sign Up
+              </Link>
             </li>
           </ul>
         </div>
-     
+      </div>
     </nav>
   );
 };
- 
-
- 
 
 export default Navbar;
